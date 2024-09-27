96b45be34cf185277eaaef4bb0436c3cdda043cc
Faiva-404 <puspafai5@gmail.com>
2024-09-27 11:50:52 +0700

    Add files via upload

---
+++ Modifikasi09.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Modifikasi09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+       String hasil = (angka % 2 ==0) ? "Ängka " + angka + "termasuk bilangan genap" : "Angka " + angka + "termasuk bilangan ganjil";
+       System.out.println(hasil);
+    }
+}
+
